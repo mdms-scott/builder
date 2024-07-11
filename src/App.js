5ee6788364dbd688950847bf0c0c2a97e4b8b631
Matt Scott <mdms.scott@gmail.com>
2024-07-10 22:40:51 -0400

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // src/App.js
 import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
 import './chota.css';
@@ -69,80 +70,82 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <h1>Army Builder</h1>
-      <select onChange={handleFactionChange} value={selectedFaction}>
-        {Object.keys(factions).map(faction => (
-          <option key={faction} value={faction}>{faction}</option>
-        ))}
-      </select>
-      <div className="container">
-        <div className="column">
-          <h2>Heroes</h2>
-          {currentFaction.heroes.map((hero, index) => (
-            <div key={index} className="unit" onClick={() => handleDisplayUnit(hero)}>
-              <p>{hero.name}</p>
-              <button onClick={(event) => handleAddUnit(hero, event)}>Add</button>
-            </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <h1>Army Builder</h1>
+        <select onChange={handleFactionChange} value={selectedFaction}>
+          {Object.keys(factions).map(faction => (
+            <option key={faction} value={faction}>{faction}</option>
           ))}
-          <h2>Units</h2>
-          {currentFaction.units.map((unit, index) => (
-            <div key={index} className="unit" onClick={() => handleDisplayUnit(unit)}>
-              <p>{unit.name}</p>
-              <button onClick={(event) => handleAddUnit(unit, event)}>Add</button>
-            </div>
-          ))}
-        </div>
-        <div className="column">
-          <h2>Unit Information</h2>
-          {displayUnit ? (
-            <div className="unit-info">
-              <p>Name: {displayUnit.name}</p>
-              <p>Unit Size: {displayUnit.unit_size}</p>
-              <p>Points: {displayUnit.points}</p>
-              {displayUnit.regiment_options && <p>Regiment Options: {displayUnit.regiment_options}</p>}
-              {displayUnit.relevant_keywords && <p>Relevant Keywords: {displayUnit.relevant_keywords}</p>}
-              {displayUnit.notes && <p>Notes: {displayUnit.notes}</p>}
-              <p>Base Size: {displayUnit.base_size}</p>
-            </div>
-          ) : (
-            <p>Select a unit to see its information</p>
-          )}
-        </div>
-        <div className="column">
-          <h2>Army List</h2>
-          <h4>Total Points: {currentTotalPoints}  Model Count: {currentModelCount}</h4>
-          <DragDropContext onDragEnd={handleDragEnd}>
-            <Droppable droppableId="selectedUnits">
-              {(provided) => (
-                <div
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                  className="unit-list"
-                >
-                  {currentSelectedUnits.map((unit, index) => (
-                    <Draggable key={index} draggableId={String(index)} index={index}>
-                      {(provided) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          className={unit.type}
-                        >
-                          <p>{unit.name} - {unit.points} pts</p>
-                          <button onClick={() => handleRemoveUnit(index)}>Remove</button>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          </DragDropContext>
+        </select>
+        <div className="container">
+          <div className="column">
+            <h2>Heroes</h2>
+            {currentFaction.heroes.map((hero, index) => (
+              <div key={index} className="unit" onClick={() => handleDisplayUnit(hero)}>
+                <p>{hero.name}</p>
+                <button onClick={(event) => handleAddUnit(hero, event)}>Add</button>
+              </div>
+            ))}
+            <h2>Units</h2>
+            {currentFaction.units.map((unit, index) => (
+              <div key={index} className="unit" onClick={() => handleDisplayUnit(unit)}>
+                <p>{unit.name}</p>
+                <button onClick={(event) => handleAddUnit(unit, event)}>Add</button>
+              </div>
+            ))}
+          </div>
+          <div className="column">
+            <h2>Unit Information</h2>
+            {displayUnit ? (
+              <div className="unit-info">
+                <p>Name: {displayUnit.name}</p>
+                <p>Unit Size: {displayUnit.unit_size}</p>
+                <p>Points: {displayUnit.points}</p>
+                {displayUnit.regiment_options && <p>Regiment Options: {displayUnit.regiment_options}</p>}
+                {displayUnit.relevant_keywords && <p>Relevant Keywords: {displayUnit.relevant_keywords}</p>}
+                {displayUnit.notes && <p>Notes: {displayUnit.notes}</p>}
+                <p>Base Size: {displayUnit.base_size}</p>
+              </div>
+            ) : (
+              <p>Select a unit to see its information</p>
+            )}
+          </div>
+          <div className="column">
+            <h2>Army List</h2>
+            <h4>Total Points: {currentTotalPoints}  Model Count: {currentModelCount}</h4>
+            <DragDropContext onDragEnd={handleDragEnd}>
+              <Droppable droppableId="selectedUnits">
+                {(provided) => (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className="unit-list"
+                  >
+                    {currentSelectedUnits.map((unit, index) => (
+                      <Draggable key={index} draggableId={String(index)} index={index}>
+                        {(provided) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            className={unit.type}
+                          >
+                            <p>{unit.name} - {unit.points} pts</p>
+                            <button onClick={() => handleRemoveUnit(index)}>Remove</button>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
